6147e5b04156639042db94beb10eee75ced706d7
deewooley <55896326+deewooley@users.noreply.github.com>
2020-03-30 11:39:18 -0400

    Complete Lambdas Exmp

--- lamdas/Main.java
+++ lamdas/Main.java
@@ -18,7 +18,29 @@ public class Main {
         }
     }
 
+    public static void usingLambdasLongForm(){
+        List<Book> books = LambdasPract.all();
+        Collections.sort(books, (Book b1, Book b2) -> {
+            return b1.getTitle().compareTo(b2.getTitle());
+        });
+        for(Book book:books){
+            System.out.println(book);
+        }
+    }
+    public static void usingLambdasShortForm(){
+        List<Book> books = LambdasPract.all();
+        Collections.sort(books, (b1, b2) ->
+             b1.getTitle().compareTo(b2.getTitle()));
+        books.forEach(book -> System.out.println(book));
+    }
+    public static void usingMethodReferences(){
+        List<Book> books = LambdasPract.all();
+        //Below Book ::or use method ... method name
+        Collections.sort(books,Comparator.comparing(Book::getTitle));
+        books.forEach( System.out::println);
+    }
     public static void main(String[] args) {
        usingAnonymousInlineCass();
+       usingLambdasLongForm();
     }
 }
